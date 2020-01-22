7ce9dde9c1690a508f822cc04d48ed46599edb29
tel3port <carn3lian@gmail.com>
2020-01-22 22:29:36 +0300

    tagged bot not really done

--- MainTaggedBot.py
+++ MainTaggedBot.py
@@ -178,7 +178,7 @@ class MainTaggedBot:
 
             new_tab = [tab for tab in self.driver.window_handles if tab != current][0]
             self.driver.switch_to.window(new_tab)
-            self.driver.find_element_by_xpath(dm_textbox_xpath).send_keys(f'{s_comp}. Free gift>! {random_lander}')
+            self.driver.find_element_by_xpath(dm_textbox_xpath).send_keys(f'{s_comp[0]}. What do you think of this? Is it legit? {random_lander}')
             time.sleep(5)
             self.driver.find_element_by_xpath(send_btn_xpath).click()
 
